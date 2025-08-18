9fbe9b99e6568b6cf1d5c768807710bcd65d728c
zano <zanocoderversion@gmail.com>
2025-08-18 21:44:04 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,9 +1,9 @@
 local supported, platform =
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Blackking-X-bobhub/KBOB/master/supportexecutor.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Blackking-X-bobhub/KBOB/refs/heads/master/supportexecutor.lua"))()
 
 if supported then
 	if game.PlaceId == 6839171747 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Blackking-X-bobhub/KBOB/master/Games/outdoors.lua"))()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/Blackking-X-bobhub/KBOB/refs/heads/master/Games/outdoors.lua'))()
 	end
 else
 	game.Players.LocalPlayer:Kick("Your executor is not supported!\nCurrent Executor: " .. platform)
